2d6d71307c72b5fb075e3c1ce2c2554c6e7c129f
sopung <jws0799@naver.com>
2022-01-18 11:44:03 +0900

    Rest API day02 - 02

--- src/main/java/co/mr/restApi02/controller/AcademyController.java
+++ src/main/java/co/mr/restApi02/controller/AcademyController.java
@@ -3,10 +3,9 @@ package co.mr.restApi02.controller;
 import co.mr.restApi02.mapper.AcademyMapper;
 import co.mr.restApi02.model.Academy;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/academy")
@@ -19,6 +18,10 @@ public class AcademyController {
     // 파라미터를  requestBody로 전달하도록 설정
     public int post(@RequestBody Academy academy) {
         return academyMapper.insert(academy);
+    }
+    @GetMapping("")
+    public List<Academy> getAcademies() {
+        return academyMapper.getAcademies();
     }
 
 }
--- src/main/java/co/mr/restApi02/mapper/AcademyMapper.java
+++ src/main/java/co/mr/restApi02/mapper/AcademyMapper.java
@@ -1,13 +1,19 @@
 package co.mr.restApi02.mapper;
 
 import co.mr.restApi02.model.Academy;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
 
 @Mapper // mapper 인식
 public interface AcademyMapper {
     @Insert("insert into academy(academy_name, academy_loc) values(#{academy.name}, #{academy.loc}")
     int insert(@Param("academy") Academy academy);
-
+    
+    @Select("select * from academy")
+    @Results({
+            @Result(property = "name", column = "academy_name"),
+            @Result(property = "loc", column = "academy_loc")
+    })
+    List<Academy> getAcademies();
 }
